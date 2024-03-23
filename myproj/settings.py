e05574f13e9d2a905832a716fe7a9752972d0cd5
QleanGG <guyguz1@gmail.com>
2024-03-23 13:53:58 +0200

    add admin settings

--- myproj/settings.py
+++ myproj/settings.py
@@ -128,10 +128,15 @@ WSGI_APPLICATION = 'myproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shop',
+        'USER': 'root',
+        'PASSWORD': '123',
+        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',  # The default port for MySQL
     }
 }
+
 
 
 # Password validation
